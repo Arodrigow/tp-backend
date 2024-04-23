21e6b1e79e02c95d66d337934f13dcccd1dce2fa
Arodrigow <rodrigow_@hotmail.com.br>
2024-04-23 10:12:04 -0300

    Added information filer to user entity

---
+++ src/users/types/serializedUser.ts
@@ -0,0 +1,25 @@
+import { Exclude } from "class-transformer";
+
+export class SerializedUser {
+
+    id: string;
+    name: string;
+    cpf:string;
+    phone_number:string;
+
+    @Exclude()
+    password:string;
+
+    @Exclude()
+    createdAt:Date;
+
+    @Exclude()
+    updatedAt:Date;
+
+    @Exclude()
+    deletedAt:Date;
+
+    constructor(partial: Partial<SerializedUser>){
+        Object.assign(this, partial);
+    }
+}
