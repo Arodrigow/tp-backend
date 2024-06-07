ea81454a3ae0cbcf6ac86b7510bd57858bb88604
Arodrigow <rodrigow_@hotmail.com.br>
2024-06-07 15:52:22 -0300

    Added search options to users

---
+++ src/users/const/searchConstants.ts
@@ -0,0 +1,3 @@
+export const userSearchConst = [
+    'cpf', 'email', 'name', 'phone_number'
+]
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import { Roles } from 'src/auth/decorator/roles.decorator';
 import { Role } from 'src/auth/enums/roles.enum';
 import { RoleGuard } from 'src/auth/guards/role.guard';
 import { CreateAdminUserDto } from './dto/create-useradmin.dto';
+import { userSearchConst } from './const/searchConstants';
 
 
 @UseInterceptors(ClassSerializerInterceptor)
@@ -82,8 +83,8 @@ export class UsersController {
     @UseGuards(AuthGuard, RoleGuard)
     async adminSearchUsers(
         @PaginationParams() paginationParams: Pagination,
-        @SortingParams(['cpf', 'email', 'name', 'deleteAt']) sort?: Sorting,
-        @FilteringParams(['cpf', 'email', 'name', 'deleteAt'])  filter?: Filtering
+        @SortingParams(userSearchConst) sort?: Sorting,
+        @FilteringParams(userSearchConst)  filter?: Filtering
     ):Promise<PaginatedResource<Partial<Users>>>{
         return await this.usersService.adminSearchUsers(paginationParams, sort, filter);
     }
